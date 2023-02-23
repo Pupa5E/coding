56633c91dc1245dd04a791c0579bca33b9b90edb
Hyunwoo Shim <hkhunk0202@icloud.com>
2023-02-23 09:10:46 +0900

    Create s20004S.c

---
+++ s20004S.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        if(30 % (i + 1) == 0)
+            printf("%d\n", i);
+    }
+    return 0;
+}
